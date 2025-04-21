778df971d1f3c0725d5a461775ce3fc463a5491f
João Vitor <joaov.lmsousa@gmail.com>
2025-04-21 19:30:37 -0300

    feat: add route to update a task

--- src/http/routes/tasks/update-task.ts
+++ src/http/routes/tasks/update-task.ts
@@ -5,59 +5,49 @@ import { z } from 'zod'
 import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
-export async function createTask(app: FastifyInstance) {
+export async function updateTask(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
     .post(
-      '/teams/:teamId/projects/:projectId/tasks',
+      '/teams/:teamId/projects/:projectId/tasks/:taskId',
       {
         schema: {
           tags: ['Tasks'],
-          summary: 'Create a task',
+          summary: 'Update a task',
           security: [{ bearerAuth: [] }],
           params: z.object({
             teamId: z.string().cuid(),
             projectId: z.string().cuid(),
+            taskId: z.string().cuid(),
           }),
           body: z.object({
             memberId: z.string().cuid(),
             description: z.string(),
-            subTasks: z.array(z.string()).optional(),
           }),
           response: {
-            201: z.null(),
+            204: z.null(),
           },
         },
       },
       async (request, reply) => {
-        const { teamId, projectId } = request.params
-        const { memberId, description, subTasks } = request.body
+        const { teamId, projectId, taskId } = request.params
+        const { memberId, description } = request.body
 
         await request.verifyUserIsTeamOwner(teamId)
 
-        const task = await prisma.task.create({
+        await prisma.task.update({
           data: {
             description,
             memberId,
+          },
+          where: {
+            id: taskId,
             projectId,
           },
         })
 
-        if (subTasks && subTasks.length > 0) {
-          const subTasksToPrisma = subTasks.map((item) => {
-            return {
-              description: item,
-              taskId: task.id,
-            }
-          })
-
-          await prisma.subTask.createMany({
-            data: subTasksToPrisma,
-          })
-        }
-
-        return reply.status(201).send()
+        return reply.status(204).send()
       }
     )
 }
